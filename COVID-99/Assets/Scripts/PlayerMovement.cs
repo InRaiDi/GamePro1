7a2663164f7da43be0565134d36d877e93078b4b
Felipe <Beraldo.felipe@gmail.com>
2020-06-08 18:25:20 -0400

    [ALT] Corrected Movement Animation
    Animation facing the way it's going

--- COVID-99/Assets/Scripts/PlayerMovement.cs
+++ COVID-99/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
+    public bool facingRight = true;
 
     Vector2 movement;
 
@@ -14,10 +16,33 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetAxis("Horizontal") < 0.0 && facingRight) //I'll do this better later.
+        {
+            Flip();
+        }
+        else if (Input.GetAxis("Horizontal") > 0.0 && !facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;      
+        
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;                
+        transform.localScale = theScale;
     }
 }
+
